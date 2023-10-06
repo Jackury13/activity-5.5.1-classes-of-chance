5066e10a0c8f1c1aa05bbcb7bb623d955a0fc286
Jackury13 <143567012+Jackury13@users.noreply.github.com>
2023-10-05 19:32:51 -0700

    finished

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
 class Casino {
-    // Write code here
-};
+constructor(name, fakeCoin){
+    this.name = name
+    this.timesplayed = 0
+    this.fakeCoin = fakeCoin
+}
+playGame(betAmount){
+    this.timesPlayed = 1
+    if(Math.random() <= 0.5 && this.fakeCoin == true){
+        console.log(this.name + ' wins!')
+    }
+    else{
+        console.log('player wins!' + betAmount * (this.timesPlayed + 1))
+    }
+   
+    }
+}
+
+
 
 // TESTS
-const myCasino = new Casino("HackerU Casino");
-console.log(myCasino);
-myCasino.playGame(5);
-myCasino.playGame(15);
-myCasino.playGame(25);
-myCasino.playGame(35);
+//const myCasino = new Casino("HackerU Casino");
+//console.log(myCasino);
+//myCasino.playGame(5);
+//myCasino.playGame(15);
+//myCasino.playGame(25);
+//myCasino.playGame(35);
 
 // BONUS TESTS
-/*
+
+
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
-*/
+
 
 // Extra BONUS TESTS
 /*
